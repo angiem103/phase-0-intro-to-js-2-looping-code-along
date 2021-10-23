ff368f12bf4663775a226a25f661115e2c510da3
Angie <amaticorena103@hotmail.com>
2021-10-23 00:49:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const names = ["Angie", "Jackie", "Robert"]
+const event = ["surprise"]
+
+function writeCards(names,event) {
+    let note = [];
+    for (let i = 0; i < names.length; i++) {
+    let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    note.push(message)};
+    return note;
+
+}
+
+function countDown(){
+    let number = 0
+    while (number <11) {
+        console.log(number++)}
+}
