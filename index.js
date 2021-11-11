c8e6422a48ba1ab58726f39633f5f881d6cc7fd0
Angie <amaticorena103@hotmail.com>
2021-11-11 10:29:12 -0500

    commit tree 813120ba72c3e41ab904cc87ac08891d323c3733

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 
-const names = ["Angie", "Jackie", "Robert"]
-const event = ["surprise"]
-
-function writeCards(names,event) {
-    let note = [];
-    for (let i = 0; i < names.length; i++) {
-    let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
-    note.push(message)};
-    return note;
-
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
 }
 
-function countDown(){
-    let number = 0
-    while (number <11) {
-        console.log(number++)}
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i< gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+let message = []
+
+function writeCards(names, party) {
+   for (let i = 0; i < names.length; i++){
+   message.push(`Thank you, ${names[i]}, for the wonderful ${party} gift!`)
+    }
+    return message
+}
+
+function countDown() {
+    let number = 10
+    while (number >= 0) {
+        console.log(number--)
+    }
 }
